5ff849b18eca704300b1e371528246b304ebf043
erinwood24 <ewood24@penguinhall.org>
2023-01-06 08:33:56 -0500

    homework

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ app.listen(41111, () => {
     console.log(`Avalanche Alley server listening at http://localhost:${config.app.port}`);
 });
 
+app.get('/avy_obs', async function (req, res) {
+    res.json("Hi there.");
+    });
+
+    const paintingData = [{painting_index: 283, season: 1, episode: 2, painting_title: 'Mt. McKinley'},{_painting_index: 284,get painting_index() {
+            return this._painting_index;
+        },
+set painting_index(value) {
+            this._painting_index = value;
+        },
+ season: 1, episode: 3, painting_title: 'Ebony Sunset'}]
