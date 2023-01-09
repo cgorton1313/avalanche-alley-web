1645e5a2926079ea7e52c505a2a73cf32420a6bb
Eloucks24 <eloucks24@penguinhall.org>
2023-01-09 10:57:44 -0500

    footer

---
+++ public/js/footer.js
@@ -0,0 +1,11 @@
+let footerHTML='
+<div class = "w3-container w3-blue-gray w3-padding-32">
+  <h4>Footer</h4>
+</div>
+
+<div class="w3-container w3-theme-l1">
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</div>
+</footer>
+'
+document.getElementById("footer").innerHTML = footerHTML
