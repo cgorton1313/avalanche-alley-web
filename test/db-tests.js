9ba72130273cf793fe6735aee3793c939533ab7c
erinwood24 <ewood24@penguinhall.org>
2023-01-09 10:32:52 -0500

    sqlRealDeal

--- test/db-tests.js
+++ test/db-tests.js
@@ -4,7 +4,7 @@ const aaData = require('../aa-data.js');
 
 async function testSomething() {
   // here you can call a function that is in aa-data.js and wait for the result
-  let someData = await aaData.sqlTest();
+  let someData = await aaData.sqlRealDeal();
   console.log(someData);
   // inspect the log to see what kind of object you got back
 }
