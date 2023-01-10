96837e82f7d3cf9503131119b45385ab6e64972d
cgortonpenguinhall <cgorton@penguinhall.org>
2023-01-10 10:13:28 -0500

    index tweaks

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -13,7 +13,7 @@ let sideHTML = `
     <a class="w3-bar-item w3-button w3-monospace w3-hover-black w3-text-blue" href="#">Study Plot Data</a>
     <a class="w3-bar-item w3-button w3-monospace w3-hover-black w3-text-blue" href="#">Field Observations</a>
     <a class="w3-bar-item w3-button w3-monospace w3-hover-black w3-text-blue" href="#">Snow Profiles</a>
-    <a class="w3-bar-item w3-button w3-monospace w3-hover-black w3-text-blue" href="#">Avalanche Observations</a>
+    <a class="w3-bar-item w3-button w3-monospace w3-hover-black w3-text-blue" href="observations.html">Avalanche Observations</a>
     <a class="w3-bar-item w3-button w3-monospace w3-hover-black w3-text-blue" href="#">Weather Observations</a>
     <a class="w3-bar-item w3-button w3-monospace w3-hover-black w3-text-blue" href="#">Maps</a>
     <a class="w3-bar-item w3-button w3-monospace w3-hover-black w3-text-blue" href="#">Videos</a>
